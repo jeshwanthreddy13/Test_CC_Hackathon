f3607f398c82f7305d32ac5f5de00762eaa216e1
J Jeshwanth Reddy <jeshwanth7899@gmail.com>
2022-03-30 19:57:53 +0530

    Final

--- Problem Statement 3/microservices/landing/app/app.py
+++ Problem Statement 3/microservices/landing/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-
+from flask_restful import Resource
 import requests
 import os
 
@@ -7,29 +7,32 @@ app = Flask(__name__)
 app.secret_key = 'thisisjustarandomstring'
 
 
+''' class Add(Resource):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def get() '''
+
+
 def add(n1, n2):
- try:
-    return int(n1) + int(n2);
- except:
-    return 0;
+    print(n1, '-', type(n1))
+    return int(n1)+int(n2)
+
 
 def minus(n1, n2):
- try:
-    return int(n1) - int(n2);
- except:
-    return 0;
+    return int(n1)-int(n2)
+
 
 def multiply(n1, n2):
- try:
-    return int(n1) * int(n2);
- except:
-    return 0;
+    return int(n1)*int(n2)
+
 
 def divide(n1, n2):
- try:
-    return int(n1) / int(n2);
- except:
-    return 0;
+    if n2 == "0":
+        return 'Division by Zero exception'
+    return int(n1)/int(n2)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -38,17 +41,37 @@ def index():
     operation = request.form.get('operation')
     result = 0
     if operation == 'add':
-        result = add(number_1, number_2)
+        result = requests.get('http://addition:5049/add/' +
+                              str(number_1)+'&'+str(number_2)).text
     elif operation == 'minus':
-        result =  minus(number_1, number_2)
+        result = requests.get('http://minus:5048/minus/' +
+                              str(number_1)+'&'+str(number_2)).text
     elif operation == 'multiply':
-        result = multiply(number_1, number_2)
+        result = requests.get('http://multiply:5047/multiply/' +
+                              str(number_1) + '&' + str(number_2)).text
     elif operation == 'divide':
-        result = divide(number_1, number_2)
-
-    flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+        result = requests.get('http://divide:5046/divide/' +
+                              str(number_1) + '&' + str(number_2)).text
+    elif operation == "bitwise_or":
+        result = requests.get('http://bitwise_or:5045/or/' +
+                              str(number_1)+'&'+str(number_2)).text
+    elif operation == "maximum":
+        result = requests.get('http://maximum:5044/maximum/' +
+                              str(number_1)+'&'+str(number_2)).text
+    elif operation == "minimum":
+        result = requests.get('http://minimum:5043/minimum/' +
+                              str(number_1)+'&'+str(number_2)).text
+    elif operation == "average":
+        result = requests.get('http://average:5042/average/' +
+                              str(number_1)+'&'+str(number_2)).text
+    elif operation == "exponential":
+        result = requests.get('http://exponential:5041/exponential/' +
+                              str(number_1)+'&'+str(number_2)).text
+    flash(
+        f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(
